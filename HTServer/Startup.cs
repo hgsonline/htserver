935f39ad27821b6e7fc1c176eef29c4c7aa64c63
saihgs <saihgs@github.com>
2017-12-28 11:19:38 +0530

    test changes in statup

--- HTServer/Startup.cs
+++ HTServer/Startup.cs
@@ -25,6 +25,7 @@ namespace HTServer
         {
             
             services.Configure<MyConfigReader>(Configuration);
+            //@sai ram
             // Not Requried as of now Install - Package MySql.Data.EntityFrameworkCore - Version 8.0.8 - 
             // for linux using docker ip addresss to be used
             // "myConnectionString": "server=192.168.43.164;port=3306; user=root; password=myroot; database=htdatabase;SslMode=none"
